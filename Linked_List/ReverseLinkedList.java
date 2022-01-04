6daf06573af9dc8f9423641224d9658185feac8d
Parth2031 <rastogiparth2011@gmail.com>
2022-01-04 15:18:22 +0530

    Question Solved in Java

--- Linked_List/ReverseLinkedList.java
+++ Linked_List/ReverseLinkedList.java
@@ -1,2 +1,129 @@
 import java.util.*;
 
+public class ReverseLinkedList
+{
+  public static Scanner scn = new Scanner(System.in);
+
+  public static class LinkedList 
+  {
+    private class Node
+    {
+      int data;
+      Node next;
+       
+      public Node() {
+
+      }
+    }
+    
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public int size() {
+      return size;
+    }
+
+    Node getNodeAt(int idx)
+    {
+      if(idx < 0 || idx >= size)
+      {
+        System.out.println("Invalid Index");
+        return null;
+      }
+      if(size == 0)
+      {
+        System.out.println("LinkedList is Empty");
+        return null;
+      }
+      Node temp = head;
+      for(int i = 1; i <= idx; i++)
+        temp = temp.next;
+      return temp;  
+    }
+    
+    void display()
+    {
+      for(Node temp = head; temp != null; temp = temp.next)
+        System.out.print(temp.data + " ");
+      System.out.println();
+    }
+
+    void addLast(int val)
+    {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = null;
+
+      if(size == 0)
+        head = tail = temp;
+      else 
+      {
+        tail.next = temp;
+        tail = temp;
+      }
+      size++;
+    }
+    
+    void ReverseLL_DataIterative()
+    {
+      int start = 0;
+      int end = size - 1;
+
+      while(start < end)
+      {
+        Node left = getNodeAt(start);
+        Node right = getNodeAt(end);
+
+        int temp = left.data;
+        left.data = right.data;
+        right.data = temp;
+
+        start++;
+        end--;
+      }
+    }
+
+    void ReverseLL_PointerIterative()
+    {
+      
+    }
+
+    void ReverseLL_Recursive()
+    {
+
+    }
+
+    void ReverseLL_PointerRecursive()
+    {
+      
+    }
+  }
+  
+  public static void main(String[] args)
+  {
+    System.out.println();
+    System.out.print("Enter the Size of Linked List: ");
+    int n = scn.nextInt();
+    LinkedList list = new LinkedList();
+    System.out.print("Enter the Values for Linked List: ");
+    for (int i = 0; i < n; i++)
+      list.addLast(scn.nextInt());
+    
+    System.out.print("Reversed Linked List by Data Iterative: ");
+    list.ReverseLL_DataIterative();
+    list.display();
+
+    System.out.print("Reversed Linked List by Pointer Iterative: ");
+    list.ReverseLL_PointerIterative();
+    list.display();
+
+    // System.out.print("Reversed Linked List by Recursive: ");
+    // list.ReverseLL_DataIterative();
+    // list.display();
+
+    // System.out.print("Reversed Linked List by Pointer Recursive: ");
+    // list.ReverseLL_DataIterative();
+    // list.display();
+  }
+}
